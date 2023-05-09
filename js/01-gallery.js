ac12d978adfaf7475b686e7f15a7b2bf84a0e2db
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-05-09 12:45:08 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 // Change code below this line
+const ref = {
+  galeryItem: document.querySelector(".gallery"),
+};
+const GalleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+ref.galeryItem.insertAdjacentHTML("afterbegin", GalleryMarkup);
+ref.galeryItem.addEventListener("click", targetImgClickHandle);
+
+// function
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `    <li class="gallery__item">
+        <a class="gallery__link" href=${original}">
+           <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </li>`;
+    })
+    .join("");
+}
+
+function targetImgClickHandle(e) {
+  e.preventDefault();
+    console.log(e.target);
+    let imgClicked = e.target.dataset.
+}
